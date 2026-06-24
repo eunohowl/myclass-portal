074cf8efbf829081d780ed02a8641c3612430fa2
eunohowl <khlazieeed@gmail.com>
2026-06-24 08:30:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'apikey')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', '')
+app.config['MAIL_SUPPRESS_SEND'] = not bool(os.getenv('MAIL_PASSWORD'))
 mail = Mail(app)
 
 def kirim_notifikasi(to_email, subject, body):
